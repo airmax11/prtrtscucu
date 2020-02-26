61d91644d56daa823f07909b8e20d973dc7090fd
Air Max <testmax200@gmail.com>
2020-02-26 12:42:53 +0100

    files_added

---
+++ configuration.ts
@@ -0,0 +1,22 @@
+import { Config } from "protractor";
+
+export let config: Config = {
+    //  seleniumAddress: "http://localhost:4444/wd/hub",
+      specs: [
+      //  "spec1.js",
+      //  "spec2.js",
+      //  "actions.js",
+      //  "frame.js",
+      "project_1/practice.js"
+     ],
+      capabilities: {
+          'browserName': 'chrome',
+          'chromeOptions': {
+             args: [
+       //   "--headless",
+          "--window-size=1600,900"
+        ],
+          'w3c': false
+          } 
+      }
+  };
---
+++ testspec.ts
@@ -0,0 +1,17 @@
+import { browser, element, by } from "protractor";
+
+describe('Protractor baby group',function() {
+    it('Open Angular js website', function() {
+        browser.get("http://juliemr.github.io/protractor-demo/");
+        element(by.model("first")).sendKeys("3");
+        element(by.model("second")).sendKeys("7");
+        element(by.id("gobutton")).click();
+        const test = element(by.css("h2[class='ng-binding']")).getText().then(function(text){
+            console.log(text);
+            return text;
+        });
+        expect(test).toBe("10");
+        console.log("Spec_1")
+    
+    });
+});
